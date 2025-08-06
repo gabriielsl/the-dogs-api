7eea9da2c45abfe87a7692fa962fea6a0652989f
Gabriel Lima <gabriiel_sl@hotmail.com>
2025-08-06 17:01:39 -0300

    Realizando a requisição GET

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,18 @@
+import { useState, useEffect } from "react";
+
 export function Card() {
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.thedogapi.com/v1/images/search?limit=5&has_breeds=1")
+      .then((response) => response.json())
+      .then((data) => setDogs(data))
+      .catch((error) => console.error("Falha na requisição: " + error));
+  }, []);
+
   return (
     <>
-      <div>
-        content
-      </div>
+      <div>content</div>
     </>
   );
 }
